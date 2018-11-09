c15693a8915eac832878bb22d5a635e79547537d
bdc5435 <bdc5435@rit.edu>
2018-11-09 17:03:10 -0500

    newW

---
+++ Test_Main.java
@@ -0,0 +1,15 @@
+public class Test_Main
+{
+   public static void main(String[] args) throws Exception
+   {
+      FootballDatabase fdb = new FootballDatabase();
+      
+      
+      fdb.loadAllDefenses();
+   
+   
+   
+   
+   }
+
+}
