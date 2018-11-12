26984966393ef058c54dffee9d9e4d9eb84fe126
Ruto <ruto@protonmail.ch>
2018-11-12 17:38:29 -0500

    Forgot to delete old files

--- Test_Main.java
+++
@@ -1,7 +0,0 @@
-public class Test_Main
-{  public static void main(String[] args) throws Exception
-   {
-      MySportsFeeds msf = new MySportsFeeds();
-      msf.getStatsByTeamPos("nyg","wr");
-   }
-}
