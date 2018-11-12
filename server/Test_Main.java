f328d05ff0d03525656fb5cb7bf1450ec0b658eb
Ruto <ruto@protonmail.ch>
2018-11-12 17:37:36 -0500

    Proposed folder structure

---
+++ server/Test_Main.java
@@ -0,0 +1,7 @@
+public class Test_Main
+{  public static void main(String[] args) throws Exception
+   {
+      MySportsFeeds msf = new MySportsFeeds();
+      msf.getStatsByTeamPos("nyg","wr");
+   }
+}
