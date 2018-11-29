5af5361b92599a7d2216af468c89ad8a617645c0
bdconnors <44237359+bdconnors@users.noreply.github.com>
2018-11-29 12:03:40 -0500

    Update Test_Main.java

--- Test_Main.java
+++ Test_Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package ritdatabaseproject;
 import ritdatabaseproject.server.models.*;
 
@@ -11,18 +10,4 @@ public class Test_Main
        fdb.loadAllCumStats();
 
    }
-
-=======
-import server.models.FootballDatabase;
-
-public class Test_Main
-{
-   public static void main(String[] args)throws Exception
-   {
-       FootballDatabase fdb = new FootballDatabase();
-        fdb.loadAllDefenseStats();
-
-   }
-
->>>>>>> d8b5dae7ef8acd4720539f72b9f88f54c8967cb4
 }
