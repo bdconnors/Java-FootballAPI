c69222f0e7de58aaba4512b30403c8bc3bba0d2e
bdc5435 <bdc5435@rit.edu>
2018-12-01 17:52:33 -0500

    added business layer stat models

---
+++ org/rit/footballapi/src/Temp_Main.java
@@ -0,0 +1,10 @@
+import main.java.datalayer.ui.stats.game.QuarterBackGameStats;
+
+public class Temp_Main {
+
+    public static void main(String[] args)throws Exception
+    {
+        QuarterBackGameStats qbgs = new QuarterBackGameStats();
+        qbgs.fetch();
+    }
+}
